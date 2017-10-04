4333d7939a22f9843dc417a877ed0286d63d8518
mitric <stojan.mitric@rt-rk.com>
2017-10-04 10:31:22 +0200

    frontend task 1 solution

---
+++ frontend_1.js
@@ -0,0 +1,26 @@
+//Frontend 1.
+var input=[{'1': ['4', 'Lorem ipsum']},{'3': ['2', 'Lorem ipsum']}];
+
+var output = new Array();
+
+function createJson() {
+  
+for(var i in input) {
+  var object = input[i];
+  
+  for(var j in object){
+        var sub_key = j;
+        var sub_val = object[j];
+       
+        var jsonObject = new Object();
+        jsonObject.question_id = sub_key;
+        jsonObject.answer_value = sub_val[0];
+        jsonObject.comments = sub_val[1];
+        output.push(jsonObject);
+  }
+}
+  
+  console.log(output);
+}
+
+createJson();
